8a13a35dd8c88fa154c0c540175995273c59e22a
miguel_betin <mbetin@corelogic.com>
2024-08-28 17:06:57 -0500

    task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -6,14 +6,14 @@ exports.handler = async (event) => {
     if (path === '/hello' && method === 'GET') {
         response = {
             statusCode: 200,
-            body: JSON.stringify({ statusCode: 200, message: 'Hello from Lambda!' })
+            body: JSON.stringify({ statusCode: 200, message: 'Hello from Lambda' })
         };
     } else {
         response = {
             statusCode: 400,
             body: JSON.stringify({ 
                 statusCode: 400,
-                message: 'Bad request syntax or unsupported method. Request path: ' + path + ' HTTP method: ' + method
+                message: 'Bad request syntax or unsupported method. Request path: ' + path + '. HTTP method: ' + method
             })
         };
     }
