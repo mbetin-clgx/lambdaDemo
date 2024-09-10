a05cec282fca8ca8e1c513dedebcfb6054ef50e4
miguel_betin <mbetin@corelogic.com>
2024-09-10 12:23:13 -0500

    task04

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
     // TODO implement
+    console.log(event);
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,8 +1,9 @@
 exports.handler = async (event) => {
     // TODO implement
+    console.log(event);
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Lambda'),
     };
     return response;
 };
