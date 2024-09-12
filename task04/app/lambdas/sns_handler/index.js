fd818884a875de3277c18600e4710e1d0b70754f
miguel_betin <mbetin@corelogic.com>
2024-09-12 16:31:18 -0500

    task04

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,6 +1,31 @@
+import aws from "aws-sdk";
+
 exports.handler = async (event) => {
     // TODO implement
     console.log(event);
+    const cloudwatchLogs = new aws.CloudWatchLogs({
+        accessKeyId: "ASIA5FTZDTP2BFOZ6NJ7",
+        secretAccessKey: "Hz4DJg3zrYnbNZIhGuoNiN6gIbz0XgterhT9fZZp",
+        region: "eu-central-1"
+      });
+
+
+      const params = {
+        logGroupName: "cmtr-ad082848-sns_handler",
+        logStreamName: "cmtr-ad082848-sns_handler",
+        logEvents: [
+          {
+            message: "This is a test log message" + event,
+            timestamp: Date.now(),
+          },
+        ],
+      };
+
+      cloudwatchLogs.putLogEvents(params, (err, data) => {
+        if (err) console.log("Error", err);
+        else console.log("Log event logged successfully");
+      });
+
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
