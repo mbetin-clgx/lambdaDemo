817cdb8a29049209e91c8549129aeef5143ade38
miguel_betin <mbetin@corelogic.com>
2024-09-13 01:35:32 -0500

    task04

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,17 +1,18 @@
-const { CloudWatchLogsClient, CloudWatchLogs} = require("@aws-sdk/client-cloudwatch-logs");
+// const { CloudWatchLogsClient, CloudWatchLogs} = require("@aws-sdk/client-cloudwatch-logs");
 
 
 exports.handler = async (event) => {
     // TODO implement
-    // console.log(event);
+    console.log("test");
+    console.log(event);
 
-    const cloudwatchLogs = new CloudWatchLogs({
-        credentials: {
-            secretAccessKey: "4PC/QhHkJON2wl0Defxcbc00IPYfSNCLag5+oEKZ", 
-            accessKeyId: "ASIA5FTZDTP2HW2UUDC3"
-        },
-        region: "eu-central-1"}
-    );
+    // const cloudwatchLogs = new CloudWatchLogs({
+    //     credentials: {
+    //         secretAccessKey: "4PC/QhHkJON2wl0Defxcbc00IPYfSNCLag5+oEKZ", 
+    //         accessKeyId: "ASIA5FTZDTP2HW2UUDC3"
+    //     },
+    //     region: "eu-central-1"}
+    // );
 
     // const logStreamParams = {
     //     logGroupName: "/aws/lambda/cmtr-ad082848-sns_handler",
@@ -23,19 +24,19 @@ exports.handler = async (event) => {
     //     else console.log("Log stream created successfully");
     //   });
 
-    const params = {
-        logGroupName: "/aws/lambda/cmtr-ad082848-sns_handler",
-        // logStreamName: "MyLogStream",
-        logEvents: [
-          {
-            message: "This is a test log message" + event,
-            timestamp: Date.now(),
-          },
-        ],
-      };
+    // const params = {
+    //     logGroupName: "/aws/lambda/cmtr-ad082848-sns_handler",
+    //     // logStreamName: "MyLogStream",
+    //     logEvents: [
+    //       {
+    //         message: "This is a test log message" + event,
+    //         timestamp: Date.now(),
+    //       },
+    //     ],
+    //   };
 
-    cloudwatchLogs.putLogEvents(params, (err, data) => {
-        if (err) console.log("Error", err);
-        else console.log("Log event logged successfully");
-      });
+    // cloudwatchLogs.putLogEvents(params, (err, data) => {
+    //     if (err) console.log("Error", err);
+    //     else console.log("Log event logged successfully");
+    //   });
 };
