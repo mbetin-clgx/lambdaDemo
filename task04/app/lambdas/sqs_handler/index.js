0be49ec3649c589770b873275ede634a0293ba11
miguel_betin <mbetin@corelogic.com>
2024-09-24 12:25:28 -0500

    task04

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -2,13 +2,16 @@
 exports.handler = async (event) => {
     // TODO implement
     console.log(JSON.stringify(event));
+    console.log(event);
 
     console.log(JSON.stringify(event.Records));
+    console.log(event.Records);
 
     const records = event.Records;
     records.forEach(record => {
 
         console.log(JSON.stringify(record));
+        console.log(record);
         console.log(record.body);
         
     });
